61d3e72ff938bd6b7ad05a89d30af361c521886f
Mitch770 <mitchzveibil@gmail.com>
2021-07-02 19:11:00 -0400

    finished most of the assignment

--- contact.rb
+++ contact.rb
@@ -1,24 +1,38 @@
 class Contact
-
+attr_reader :email
+attr_writer :contact
+@@contacts= []
+@@id = 1
   # This method should initialize the contact's attributes
-  def initialize
-
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
   end
 
   # This method should call the initializer, 
   # store the newly created contact, and then return it
-  def self.create
-
+  def self.create(first_name, last_name, email, note)
+  new_contact = Contact.new(first_name, last_name, email, note)
+  @@contacts << new_contact
+  @id = @@id
+  @@id += 1
+  return new_contact
   end
 
   # This method should return all of the existing contacts
   def self.all
-
+    @@contacts
   end
 
   # This method should accept an id as an argument
   # and return the contact who has that id
-  def self.find
+  def self.find(id)
+    @@contacts.each do |id|
+      if id == @id 
+        return @@contacts[id]
+      end
 
   end
 
@@ -35,24 +49,30 @@ class Contact
   # by specifying both the name of the attribute and the value
   # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
   def self.find_by
-
+    @@contacts.each do |first_name|
+      if first_name == @first_name 
+        return @@contacts[first_name] # have to fix this method
+      end
   end
 
   # This method should delete all of the contacts
   def self.delete_all
-
+   # delete.@@contacts
   end
 
   def full_name
-
+    @full_name
   end
 
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
   def delete
-
+    delete.new_contact
   end
 
   # Feel free to add other methods here, if you need them.
   
 end
+
+
+
